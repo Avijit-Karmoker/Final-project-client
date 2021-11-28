93fb341efb35aa2e477ad73f829631a134998b1b
DESKTOP-RUUJRUB\Ovijit <ovijit1hsc@gmail.com>
2021-11-28 20:37:59 +0600

    add some element in header part

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import style from '../../styles/Header.module.css'
+import style from "../../styles/Header.module.css";
 
 const Header = () => {
   return (
@@ -10,6 +10,7 @@ const Header = () => {
           id="carouselExampleIndicators"
           className="carousel slide"
           data-bs-ride="carousel"
+          style={{ lineHeight: ".8em" }}
         >
           <div className="carousel-indicators">
             <button
@@ -41,29 +42,63 @@ const Header = () => {
                   alt="Background Image 1"
                   width={1920}
                   height={900}
-                  opacity={.5}
                 />
+                <div
+                  className="carousel-caption d-none d-md-block"
+                  style={{ top: "20%" }}
+                >
+                  <Image
+                    src="/images/fork.png"
+                    alt="Fork"
+                    width={100}
+                    height={100}
+                    className={style.icon}
+                  />
+                  <h3>We Don't Just Cook</h3>
+                  <h1>We Make Delicious Cook</h1>
+                  <p>
+                    Granny is a restaurant, bar and coffee roastery located on
+                    Egypt. We have
+                    <br />
+                    awesome recipes and the most talented chefs in town!
+                  </p>
+                </div>
               </div>
             </div>
             <div className="carousel-item">
-              <div className={style.item1}>
+              <div className={style.item2}>
                 <Image
                   src="/images/Image-2.png"
                   alt="Background Image 2"
                   width={1920}
                   height={900}
                 />
+                <div
+                  className="carousel-caption d-none d-md-block"
+                  style={{ top: "35%" }}
+                >
+                  <h4>Creative & Talanted Chefs</h4>
+                  <h1>Hot And Ready To Serve</h1>
+                  <button className={style.button}>Reserve Now</button>
+                </div>
               </div>
             </div>
             <div className="carousel-item">
-              <div className={style.item1}>
+              <div className={style.item3}>
                 <Image
                   src="/images/Image-3.png"
                   alt="Background Image 3"
                   width={1920}
                   height={900}
-                  z-index="-1"
                 />
+                <div
+                  className="carousel-caption d-none d-md-block"
+                  style={{ top: "45%" }}
+                >
+                  <h3>Creative & Talanted Chefs</h3>
+                  <h1>Hot And Ready To Serve</h1>
+                  <button>Reserve Now</button>
+                </div>
               </div>
             </div>
           </div>
